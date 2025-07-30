4b261e4a48c2b8ecd62468d96e7610c9beba8ef2
Виталий Лавшонок <114582703+valavshonok@users.noreply.github.com>
2025-07-30 10:40:16 +0300

    Added technocup logo

--- cf-dark-mode.js
+++ cf-dark-mode.js
@@ -1731,6 +1731,17 @@ body.cf-theme img[src="//assets.codeforces.com/images/star-vkcup-2016-300.png"] 
     visibility: hidden;
 }
 
+/* technocup */
+body.cf-theme a:has(>img[src="//assets.codeforces.com/images/technocup-logo-en.png"]) {
+    background-image: url(https://codeforces.com/predownloaded/5b/92/5b929b12e16a75a85d0a593aea162226d72662e8.png);
+    background-size: cover;    
+    display: flex;
+}
+body.cf-theme img[src="//assets.codeforces.com/images/technocup-logo-en.png"] {
+    visibility: hidden;
+}
+
+
 
 /* 
 
